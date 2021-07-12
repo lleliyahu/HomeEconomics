6dd51d920cdbb11720b38e49ee413edab43d5315
lleliyahu <lleliyahu@gmail.com>
2021-07-12 22:41:38 +0300

    add new

---
+++ home-economics/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+  <router-link to="/about">about</router-link>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    //
+  }),
+};
+</script>
